d46d22e2941c936ecb16e8ee61b4bef384922507
Greg Lixandrão <contato@greglixandrao.com>
2025-08-05 08:45:50 -0300

    function: add information in function files

--- 01 - Estrutura de dados/05 - Funções/02_argumentos_nomeados.py
+++ 01 - Estrutura de dados/05 - Funções/02_argumentos_nomeados.py
@@ -1,8 +1,8 @@
-def salvar_carro(marca, modelo, ano, placa):
+def salvar_carro(ano, marca, modelo, placa):
     # salva carro no banco de dados...
     print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
 
 
 salvar_carro("Fiat", "Palio", 1999, "ABC-1234")
 salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1234")
-salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234"})
+salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1234"}) # irá receber os valores como argumentos nomeados e recebe como dicionário (**kwargs) ou tuplas (*args)
--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -6,6 +6,7 @@ def exibir_poema(data_extenso, *args, **kwargs):
 
 
 exibir_poema(
+    "Sex, 13 de out de 2023",
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
