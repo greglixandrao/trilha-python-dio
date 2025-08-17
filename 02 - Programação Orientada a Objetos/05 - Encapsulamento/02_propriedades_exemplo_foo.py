cb726eabeaccabb4aecc2256e8f2563c403f6d66
Greg Lixandrão <contato@greglixandrao.com>
2025-08-17 20:24:39 -0300

    modify polimorf a properties files

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
@@ -6,18 +6,21 @@ class Foo:
     def x(self):
         return self._x or 0
 
+    # Setter para modificar o valor de x
     @x.setter
     def x(self, value):
         self._x += value
 
+    # Pouco usado - Só quando você não quer deletar o atributo diretamente
+    # Exemplo: quando você quer resetar o valor de x para um valor padrão
     @x.deleter
     def x(self):
-        self._x = 0
-
+        self._x = -1
+        # del self._x - # Não deletar, mas sim reatribuir um valor padrão
 
 foo = Foo(10)
 print(foo.x)
-del foo.x
-print(foo.x)
 foo.x = 10
+print(foo.x)
+del foo.x
 print(foo.x)
