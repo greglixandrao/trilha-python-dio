3eec8737fcd26b9a2be32cfca7d51d165cbf4e8d
Greg Lixandrão <contato@greglixandrao.com>
2025-08-18 08:57:57 -0300

    Comment in class and instance atributes file

--- 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
+++ 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
@@ -18,6 +18,9 @@ aluno_1 = Estudante("Guilherme", 1)
 aluno_2 = Estudante("Giovanna", 2)
 mostrar_valores(aluno_1, aluno_2)
 
-Estudante.escola = "Python"
+Estudante.escola = "Python" # Atribuindo valor de escola para todos os alunos
 aluno_3 = Estudante("Chappie", 3)
+mostrar_valores(aluno_1, aluno_2, aluno_3)
+
+aluno_1.escola = "Linuxtips" # Atribuindo valor de escola somente para o aluno_1
 mostrar_valores(aluno_1, aluno_2, aluno_3)
